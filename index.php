575db21cfc7d206c7d5d4817612de2ff9b2e6396
NetraGhaisas <netraghaisas@gmail.com>
2019-10-05 19:08:35 +0530

    File read

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once("test.html"); ?>
+<?php 
+// include_once("test.html");
+$basic_info = fopen("php://output", 'w');
+header("Content-disposition: attachment; filename=test.csv");
+header("Content-Type: text/csv");
+header('Content-Length: ' . filesize("php://output"));
+readfile("php://output");
+?>
